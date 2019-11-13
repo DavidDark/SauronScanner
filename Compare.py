4eda4d3f6172372760e22dd7c12af2a865a58113
DavidDark <link100david@hotmail.com>
2019-11-13 14:54:32 -0600

    Commit 13_11_19

--- Compare.py
+++ Compare.py
@@ -1,41 +1,35 @@
 import cv2
 import numpy as np
-import glob
-import os
-import shutil
-#import matplotlib.pyplot as plt
-#import matplotlib.image as mpimg
+#Para los soniditos
+from pydub import AudioSegment
+from pydub.playback import play
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-if not os.path.exists("Resultados"):
-	os.mkdir("Resultados")
-else:
-	try:
-		shutil.rmtree("Resultados")
-		os.mkdir("Resultados")
-	except OSError as e:
-		print ("Error: %s - %s." % (e.filename, e.strerror))
-
-
-#res= len(glob.glob("Resultados/*.jpg"))
-ttl= len(glob.glob("Muestras/*.jpg"))
-imagen1 = cv2.imread("Correcto.jpg")
-
-for i in range(0,ttl):
-
-	imagen2 = cv2.imread("Muestras/temp_{}.jpg".format(i))
+def comparar(ttl):
+	ttl= ttl -1
+	
+	imagen1 = cv2.imread("Correcto.jpg")
+	imagen2 = cv2.imread("Muestras/temp_{}.jpg".format(ttl))
 
 	difference = cv2.subtract(imagen1, imagen2)
 
 	result = not np.any(difference) #if difference is all zeros it will return False
-	
+		
 	if result is True:
-		print ("Las imagen {} es idéntica.".format(i))
+			print ("Las imagen {} es idéntica.".format(ttl))
 
 	else:
+			
+		cv2.imwrite("Resultados/temporal_{}.jpg".format(ttl), difference)
+		#Codigo para el sonidito
+		fname = "/home/nimperii/SauronScanner/OOT_Navi_Hello1.wav"
+		mysong = AudioSegment.from_mp3(fname)
+		play(mysong)
+
+		print ("Las imagen {} es diferente.".format(ttl))
 		
-		cv2.imwrite("Resultados/temporal_{}.jpg".format(i), difference)
-		print ("Las imagen {} es diferente.".format(i))
-	
-		#img=mpimg.imread("Resultados/temporal_{}.jpg".format(res))
-		#imgplot = plt.imshow(img)
-		#plt.show()
+		img=mpimg.imread("Resultados/temporal_{}.jpg".format(ttl))
+		imgplot = plt.imshow(img)
+		plt.show()
+	return("Done")
--- scanner.py
+++ scanner.py
@@ -1,16 +1,36 @@
-#Scanner.py
-import cv2
-import numpy as np
+import os
+import glob
+import configparser
+import Compare
+import setup
 
-img =cv2.imread("Correcto.jpg")
-height, width = img.shape[:2]
 
-#Coordinates
-start_row, start_col = int(height*.5), int(width*.5)
+#Comprueba si existen las carpetas, si no existen, las crea
+stp= setup.stp()
+print(stp)
 
-end_row, end_col = int(height*.95), int(width*.95)
+while 1:
+	#Toma como variable la cantidad de muestras en la carpeta
+	ttl= len(glob.glob("Muestras/*.jpg"))
 
-crop = img[start_row:end_row, start_col:end_col]
+	#Toma como variable el valor que se encuentra en el archivo init
+	#Como si fuera un cache con el valor anterior de ttl
+	cache= configparser.ConfigParser()
+	cache.read("config.ini")
+	cnum = cache.getint("var", "ttl")
+	if ttl == 0:
+		continue
+	else:
+		#Compara si la cant en la carpeta es diferente al cache
+		if ttl != cnum:
+			#Si es diferente ejecuta las comparaciones de muestra
+			res= Compare.comparar(ttl)
+			cnums= str(ttl)
+			cache.set('var', 'ttl', cnums)
+			#Y actualiza el valor en el cache por el valor actual
+			with open('config.ini', 'w') as configfile:
+				cache.write(configfile)
 
-cv2.imshow('Crop', crop)
-cv2.waitKey(0)
+			print(res)
+		else:
+			continue
